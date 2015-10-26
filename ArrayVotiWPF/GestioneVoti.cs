759d3a4e203d17e11eda800cb297513ab0d72b0e
Michele Brunelli <michele.brunelli@marconirovereto.it>
2015-10-26 09:50:07 +0100

    Implemento inserisci voto e media

---
+++ ArrayVotiWPF/GestioneVoti.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Globalization;
+
+namespace GestioneVoti
+{
+    public enum TipoVoto
+    {
+        Orale = 0,
+        Scritto = 1,
+        Pratico = 2,
+    }
+
+    public enum Materia
+    {
+        Italiano = 0,
+        Inglese = 1,
+        Informatica = 2,
+        Matematica = 3,
+        Telecomunicazioni = 4,
+        Tecnologie = 5,
+        Sistemi = 6,
+        Ginnastica = 7,
+    }
+
+
+    public enum TipoVoce
+    {
+        Voto = 0,
+        Materia = 1,
+        Tipo = 2,
+        Data = 3,
+
+    }
+
+    public class Voto
+    {
+        public float Valore { get; set; }
+        public TipoVoto Tipo { get; set; }
+        public DateTime Data { get; set; }
+        public Materia Materia { get; set; }
+
+        public Voto(Materia materia, TipoVoto tipo, float voto, DateTime data)
+        {
+            Valore = voto;
+            Tipo = tipo;
+            Data = data;
+            Materia = materia;
+        }
+    }
+    public class Voti
+    {
+        public int DimensioneMassima { get; set; }
+        private readonly Voto[] _voti;
+        private int _dimensione = 0;
+
+        public Voti()
+            : this(50)
+        {
+            Popola();
+        }
+
+        public Voti(int dimensioneMassima)
+        {
+            DimensioneMassima = dimensioneMassima;
+            _voti = new Voto[DimensioneMassima];
+            _dimensione = 0;
+        }
+        //public string[] DammiIVoti()
+        //{
+        //    string[] voti = new string[_dimensione];
+        //    for (var i = 0; i < _dimensione; i++)
+        //        voti[i] = _voti[i].Valore.ToString("F1",CultureInfo.CurrentCulture);
+        //    return voti;
+        //}
+
+        public Voto[] DammiIVoti()
+        {
+            return _voti;
+        }
+
+        private void Popola()
+        {
+            _voti[0] = new Voto(0, TipoVoto.Scritto, 5, DateTime.Parse("21/9/2015"));
+            _voti[1] = new Voto(0, TipoVoto.Pratico, 4, DateTime.Parse("18/09/2015"));
+            _voti[2] = new Voto(0, TipoVoto.Orale, 7, DateTime.Parse("16/09/2015"));
+            _dimensione = 3;
+        }
+
+        public Voti InserisciVoto(Materia materia, TipoVoto tipo, float voto, DateTime dataora)
+        {
+            if (_dimensione <= DimensioneMassima)
+            {
+                _voti[_dimensione] = new Voto(materia, tipo, voto, dataora);
+                _dimensione++;
+                return this;
+            }
+            return null;
+        }
+
+        public Voti CancellaVoto(int posizione)
+        {
+            if (!IndiceValido(posizione)) return null;
+            for (var i = posizione - 1; i < _dimensione; i++)
+            {
+                _voti[i] = _voti[i + i];
+                _dimensione--;
+            }
+            return this;
+        }
+
+        public float Media(Materia materia)
+        {
+            float sum = 0;
+            int dim = 0;
+            for (var i = 0; i < _dimensione; i++)
+            {
+                if (_voti[i].Materia == materia)
+                {
+                    sum += _voti[i].Valore;
+                    dim++;
+                }
+            }
+            return dim == 0 ? 0 : sum / dim;
+        }
+
+        private bool IndiceValido(int i)
+        {
+            return i >= 0 && i <= _dimensione && _dimensione > 0;
+        }
+        private int Confronta(TipoVoce voce, int i, int k)
+        {
+            if (!IndiceValido(i)) throw new ArgumentOutOfRangeException("i", i, null);
+            if (!IndiceValido(k)) throw new ArgumentOutOfRangeException("k", k, null);
+            switch (voce)
+            {
+                case TipoVoce.Voto:
+                    return (_voti[i].Valore < _voti[k].Valore) ? -1 : (_voti[i].Valore > _voti[k].Valore) ? 1 : 0;
+                    break;
+                case TipoVoce.Tipo:
+                    return (_voti[i].Tipo < _voti[k].Tipo) ? -1 : (_voti[i].Tipo > _voti[k].Tipo) ? 1 : 0;
+                    break;
+                case TipoVoce.Data:
+                    return (_voti[i].Data < _voti[k].Data) ? -1 : (_voti[i].Data > _voti[k].Data) ? 1 : 0;
+                    break;
+                case TipoVoce.Materia:
+                    return (_voti[i].Materia.CompareTo(_voti[k].Materia));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("voce", voce, null);
+            }
+        }
+
+        private Voti Scambia(int i, int k)
+        {
+            if (IndiceValido(i) && IndiceValido(k))
+            {
+                var voto = _voti[i];
+                _voti[i] = _voti[k];
+                _voti[k] = voto;
+                return this;
+            }
+            return null;
+
+        }
+        public Voti Ordina(TipoVoce voce)
+        {
+            for (var i = 0; i < _dimensione; i++)
+            {
+                var minimo = i;
+                for (var j = i + 1; j < _dimensione; j++)
+                {
+                    if (Confronta(voce, j, i) < 0)
+                        minimo = j;
+                }
+                if (minimo != i)
+                    Scambia(minimo, i);
+            }
+            return this;
+        }
+
+    }
+}
---
+++ ArrayVotiWPF/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GestioneVoti;
+
+namespace ArrayVotiWPF
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GestioneVoti.Voti Voti { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //uxTipoComboBox.Items.Clear(); 
+            //foreach (var tipo in Enum.GetNames(typeof(TipoVoto)))
+            //{
+            //    uxTipoComboBox.Items.Add(tipo);
+
+            //}
+            UxTipoComboBox.ItemsSource = Enum.GetValues(typeof(TipoVoto)).Cast<TipoVoto>();
+            UxMediaComboBox.ItemsSource = Enum.GetValues(typeof(Materia)).Cast<Materia>();
+            UxMateriaComboBox.ItemsSource = Enum.GetValues(typeof(Materia)).Cast<Materia>();
+
+
+
+            Voti = new Voti();
+            UxDatiListView.ItemsSource = Voti.DammiIVoti();
+            //var items = new Voto[3];
+            //            items[0] = new Voto(TipoVoto.Orale, 5, DateTime.Now );
+            //            items[1] = new Voto(TipoVoto.Orale, 6, DateTime.Now );
+            //            items[2] = new Voto(TipoVoto.Scritto, 7, DateTime.Now );
+            //items = this.Voti.DammiIVoti();
+            //            UxDatiListView.ItemsSource = items;
+
+        }
+
+        private void VisualizzaVoti(Voti voti)
+        {
+
+        }
+
+        private void MenuNuovo_Click(object sender, RoutedEventArgs e)
+        {
+            UxInserisciVotoButton.IsEnabled = true;
+        }
+
+        private void MenuAiuto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UxInserisciVotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            float voto;
+            float.TryParse(UxVotoTextBox.Text, out voto);
+            Voti.InserisciVoto((Materia)UxMateriaComboBox.SelectedItem, (TipoVoto)UxTipoComboBox.SelectedItem, voto, DateTime.Now);
+            Voto[] voti = this.Voti.DammiIVoti();
+            UxDatiListView.ItemsSource = null;
+            UxDatiListView.ItemsSource = voti;
+        }
+
+        private void MenuNuovoRegistro_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UxVoto_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+            }
+        }
+
+
+        private void UxDatiListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void UxVotoHeader_Click(object sender, RoutedEventArgs e)
+        {
+            Voti.Ordina(TipoVoce.Voto);
+            UxDatiListView.ItemsSource = null;
+            UxDatiListView.ItemsSource = Voti.DammiIVoti();
+        }
+
+        private void UxDataHeader_Click(object sender, RoutedEventArgs e)
+        {
+            Voti.Ordina(TipoVoce.Data);
+            UxDatiListView.ItemsSource = null;
+            UxDatiListView.ItemsSource = Voti.DammiIVoti();
+
+        }
+
+        private void UxTipoHeader_Click(object sender, RoutedEventArgs e)
+        {
+            Voti.Ordina(TipoVoce.Data);
+            UxDatiListView.ItemsSource = null;
+            UxDatiListView.ItemsSource = Voti.DammiIVoti();
+
+        }
+
+        private void UxMateriaHeader_Click(object sender, RoutedEventArgs e)
+        {
+            Voti.Ordina(TipoVoce.Materia);
+            UxDatiListView.ItemsSource = null;
+            UxDatiListView.ItemsSource = Voti.DammiIVoti();
+
+        }
+
+        private void BottoneMedia_Click(object sender, RoutedEventArgs e)
+        {
+            float valore = 0;
+            Materia media = (Materia)UxMediaComboBox.SelectedItem;
+            valore = this.Voti.Media(media);
+            MediaTextBox.Text = Convert.ToString(valore);
+        }
+
+        private void UxDatiListView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
+
